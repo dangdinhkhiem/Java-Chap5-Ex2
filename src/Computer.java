e17e03efcb4ff0061d8c3ae4ce375a72f994dcd6
dangdinhkhiem <157661911+dangdinhkhiem@users.noreply.github.com>
2024-03-21 07:19:22 +0700

    Update Computer.java

--- src/Computer.java
+++ src/Computer.java
@@ -1,2 +1,14 @@
-public class Computer {
+public abstract class computer {
+    public String name;
+
+    protected String ID;
+    float price;
+    abstract void showinfo();
+
+    public computer(String name, String ID, float price) {
+        this.name = name;
+        this.ID = ID;
+        this.price = price;
+    }
+
 }
